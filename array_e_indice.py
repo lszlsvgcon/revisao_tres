27026f50c5d5623649c1787414c230bf67eb067c
lszlsvgcon <lsfmaillsvl@gmail.com>
2024-04-04 22:50:56 -0300

    array-nomes

---
+++ array_e_indice.py
@@ -0,0 +1,13 @@
+#1. Crie uma função que declara um array com 4 nomes diferentes e imprime cada um deles um embaixo do outro. Ao final faça um commit no GitHub.
+
+#Exemplo de entrada: Nenhuma Exemplo de saída:
+
+#1-João 2-Maria 3-Fulano 4-Ciclano
+
+
+lista_nomes = ['João', 'Maria', 'Fulano', 'Ciclano']
+
+for i in range(4) :
+    print(lista_nomes[i])
+    
+    
