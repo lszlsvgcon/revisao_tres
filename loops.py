448975fa8d0917be3769f6b6ad00add13d8d7375
lszlsvgcon <lsfmaillsvl@gmail.com>
2024-04-07 20:03:23 -0300

    loop- contagem de 0 a 20

---
+++ loops.py
@@ -0,0 +1,12 @@
+#5. Crie uma função que imprime uma contagem de 0 a 20. Ao final faça um commit no GitHub.
+#Exemplo de entrada: Nenhuma Exemplo de saída: 0 1 2
+
+def imprimir_contagem():
+    for i in range(21):
+        print(i)
+
+# Chamando a função para imprimir a contagem
+imprimir_contagem()
+
+
+
